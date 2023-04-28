76799cc61bdc96fda483e23ef956ffe5f8702322
Savin Shetty <savinshetty@192.168.1.8>
2023-04-29 01:05:41 +0530

    Initial commit

---
+++ testAPI.py
@@ -0,0 +1,11 @@
+import requests
+
+v_data = requests.get("http://www.techsckool.com/")
+
+# print(v_data)
+# print(v_data.status_code)
+# print(v_data.text)
+# print(v_data.headers)
+print(v_data.url)
+# print(v_data.raw)
+# print(v_data.content)
